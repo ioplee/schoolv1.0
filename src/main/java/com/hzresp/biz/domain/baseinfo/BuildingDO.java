673a7e2781714e58a20f02da6b815d5d1eea7fc4
robin <15372591185@189.cn>
2016-05-10 18:00:24 +0800

    楼宇实体更新

--- src/main/java/com/hzresp/biz/domain/baseinfo/BuildingDO.java
+++ src/main/java/com/hzresp/biz/domain/baseinfo/BuildingDO.java
@@ -17,121 +17,19 @@ package com.hzresp.biz.domain.baseinfo;
  */
 public class BuildingDO {
 	/** The building_no. */
-	private long building_no;
+	private long buildingNo;
 	
 	/** The building_name. */
-	private String building_name;
+	private String buildingName;
 	
 	/** The customer_no. */
-	private long customer_no;
+	private long customerNo;
 	
 	/** The memo. */
 	private String memo;
 	
 	/** The building_status. */
-	private String building_status;
+	private String buildingStatus;
 	
-	/** The gmt_create. */
-	private String gmt_create;
 	
-	/** The gmt_modify. */
-	private String gmt_modify;
-
-	/**
-	 * @return the building_no
-	 */
-	public long getBuilding_no() {
-		return building_no;
-	}
-
-	/**
-	 * @param building_no the building_no to set
-	 */
-	public void setBuilding_no(long building_no) {
-		this.building_no = building_no;
-	}
-
-	/**
-	 * @return the building_name
-	 */
-	public String getBuilding_name() {
-		return building_name;
-	}
-
-	/**
-	 * @param building_name the building_name to set
-	 */
-	public void setBuilding_name(String building_name) {
-		this.building_name = building_name;
-	}
-
-	/**
-	 * @return the customer_no
-	 */
-	public long getCustomer_no() {
-		return customer_no;
-	}
-
-	/**
-	 * @param customer_no the customer_no to set
-	 */
-	public void setCustomer_no(long customer_no) {
-		this.customer_no = customer_no;
-	}
-
-	/**
-	 * @return the memo
-	 */
-	public String getMemo() {
-		return memo;
-	}
-
-	/**
-	 * @param memo the memo to set
-	 */
-	public void setMemo(String memo) {
-		this.memo = memo;
-	}
-
-	/**
-	 * @return the building_status
-	 */
-	public String getBuilding_status() {
-		return building_status;
-	}
-
-	/**
-	 * @param building_status the building_status to set
-	 */
-	public void setBuilding_status(String building_status) {
-		this.building_status = building_status;
-	}
-
-	/**
-	 * @return the gmt_create
-	 */
-	public String getGmt_create() {
-		return gmt_create;
-	}
-
-	/**
-	 * @param gmt_create the gmt_create to set
-	 */
-	public void setGmt_create(String gmt_create) {
-		this.gmt_create = gmt_create;
-	}
-
-	/**
-	 * @return the gmt_modify
-	 */
-	public String getGmt_modify() {
-		return gmt_modify;
-	}
-
-	/**
-	 * @param gmt_modify the gmt_modify to set
-	 */
-	public void setGmt_modify(String gmt_modify) {
-		this.gmt_modify = gmt_modify;
-	}
 }
---
+++ src/main/java/com/hzresp/biz/domain/baseinfo/BuildingLevelDO.java
@@ -0,0 +1,42 @@
+/**   
+* @Title: BuildingLevelDO.java 
+* @Package com.hzresp.biz.domain.baseinfo 
+* @Description: TODO(用一句话描述该文件做什么) 
+* @author robin
+* @date 2016年5月10日 下午5:58:04 
+* @version V1.0   
+*/
+package com.hzresp.biz.domain.baseinfo;
+
+/** 
+ * @ClassName: BuildingLevelDO 
+ * @Description: 楼层实体bean
+ * @author robin
+ * @date 2016年5月10日 下午5:58:04 
+ *  
+ */
+public class BuildingLevelDO {
+	/** The level_no. */
+	private long level_no;
+	
+	/** The building_no. */
+	private long building_no;
+	
+	/** The memo. */
+	private String memo;
+	
+	/** The level_status. */
+	private String level_status;
+	
+	/** The gmt_createl. */
+	private String gmt_create;
+	
+	/** The gmt_modify. */
+	private String gmt_modify;
+	
+	/** The customer_no. */
+	private String customer_no;
+	
+	/** The building_name. */
+	private String building_name;
+}
