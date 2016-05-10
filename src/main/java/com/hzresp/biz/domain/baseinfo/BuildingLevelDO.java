ddc62187f009ae886a7ea1696641dddf1f85e67b
robin <15372591185@189.cn>
2016-05-10 18:01:55 +0800

    楼宇

--- src/main/java/com/hzresp/biz/domain/baseinfo/BuildingLevelDO.java
+++ src/main/java/com/hzresp/biz/domain/baseinfo/BuildingLevelDO.java
@@ -17,26 +17,88 @@ package com.hzresp.biz.domain.baseinfo;
  */
 public class BuildingLevelDO {
 	/** The level_no. */
-	private long level_no;
+	private long levelNo;
 	
 	/** The building_no. */
-	private long building_no;
+	private long buildingNo;
 	
 	/** The memo. */
 	private String memo;
 	
 	/** The level_status. */
-	private String level_status;
-	
-	/** The gmt_createl. */
-	private String gmt_create;
-	
-	/** The gmt_modify. */
-	private String gmt_modify;
+	private String levelStatus;
 	
 	/** The customer_no. */
-	private String customer_no;
-	
-	/** The building_name. */
-	private String building_name;
+	private String customerNo;
+
+	/**
+	 * @return the levelNo
+	 */
+	public long getLevelNo() {
+		return levelNo;
+	}
+
+	/**
+	 * @param levelNo the levelNo to set
+	 */
+	public void setLevelNo(long levelNo) {
+		this.levelNo = levelNo;
+	}
+
+	/**
+	 * @return the buildingNo
+	 */
+	public long getBuildingNo() {
+		return buildingNo;
+	}
+
+	/**
+	 * @param buildingNo the buildingNo to set
+	 */
+	public void setBuildingNo(long buildingNo) {
+		this.buildingNo = buildingNo;
+	}
+
+	/**
+	 * @return the memo
+	 */
+	public String getMemo() {
+		return memo;
+	}
+
+	/**
+	 * @param memo the memo to set
+	 */
+	public void setMemo(String memo) {
+		this.memo = memo;
+	}
+
+	/**
+	 * @return the levelStatus
+	 */
+	public String getLevelStatus() {
+		return levelStatus;
+	}
+
+	/**
+	 * @param levelStatus the levelStatus to set
+	 */
+	public void setLevelStatus(String levelStatus) {
+		this.levelStatus = levelStatus;
+	}
+
+	/**
+	 * @return the customerNo
+	 */
+	public String getCustomerNo() {
+		return customerNo;
+	}
+
+	/**
+	 * @param customerNo the customerNo to set
+	 */
+	public void setCustomerNo(String customerNo) {
+		this.customerNo = customerNo;
+	}
+
 }
